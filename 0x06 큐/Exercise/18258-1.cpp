69442b1d083758089e46d159f1f13cf2da558609
HyunUk Kwon <113490741+woogie01@users.noreply.github.com>
2023-06-10 18:14:39 +0900

    STL Queue (N이 클 때)

---
+++ 0x06 큐/Exercise/18258-1.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <queue>
+
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int N;
+    cin >> N;
+
+    return 0;
+}
