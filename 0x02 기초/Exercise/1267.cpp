610235827d13be4588a25a76ad49116a25bfe5db
HyunUk Kwon <113490741+woogie01@users.noreply.github.com>
2023-06-20 13:29:48 +0900

    핸드폰 요금

---
+++ 0x02 기초/Exercise/1267.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int result, input;
+string str = "DCBAE";
+
+int main(void)
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    for (int r = 0; r < 3; r++)
+    {
+        result = 0;
+        for (int c = 0; c < 4; c++)
+        {
+            cin >> input;
+            result += input;
+        }
+        cout << str[result] << '\n';
+    }
+}
